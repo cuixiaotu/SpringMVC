494b2294d2af79c9373410d67cca25e076cfbe47
theodore <cuixin1974@stnts.com>
2022-06-30 11:48:53 +0800

    第五章完结

--- SpringMVC-demo3/src/main/java/com/xiaotu3/mvc/controller/ScopeController.java
+++ SpringMVC-demo3/src/main/java/com/xiaotu3/mvc/controller/ScopeController.java
@@ -2,16 +2,63 @@ package com.xiaotu3.mvc.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Controller
 public class ScopeController {
 
     @RequestMapping("/testRequestByServletAPI")
     public String testRequestByServletAPI(HttpServletRequest request){
-        request.setAttribute("testRequestScope","Hello,servelAPI");
+        request.setAttribute("testScope","Hello,servelAPI");
+        return "success";
+    }
+
+    @RequestMapping("/testModelAndView")
+    public ModelAndView testModelAndView(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("testScope","hello,modelAndView");
+        modelAndView.setViewName("success");
+        return modelAndView;
+    }
+
+    @RequestMapping("/testModel")
+    public String testModel(Model model){
+        model.addAttribute("testScope","hello,model");
+        return "success";
+    }
+
+    @RequestMapping("/testMap")
+    public String testMap(Map<String,Object> map){
+        map.put("testScope","hello,map");
+        return "success";
+    }
+
+    @RequestMapping("/testModelMap")
+    public String testModelMap(ModelMap modelMap){
+        modelMap.addAttribute("testScope","hello.modelMap");
+        return "success";
+    }
+
+
+    @RequestMapping("/testSession")
+    public String testSession(HttpSession session){
+        session.setAttribute("testSession","hello,session");
+        return "success";
+    }
+
+
+    @RequestMapping("/testApplication")
+    public String testApplication(HttpSession session){
+        ServletContext application = session.getServletContext();
+        application.setAttribute("testApplication","hello,application");
         return "success";
     }
 
