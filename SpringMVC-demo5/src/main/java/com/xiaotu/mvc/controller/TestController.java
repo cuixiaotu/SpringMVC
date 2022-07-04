7e022981f194a096ec84a95cfbdee62c9de62f1a
theodore <cuixin1974@stnts.com>
2022-07-04 15:11:52 +0800

    第十一章 拦截器interceptor完结

---
+++ SpringMVC-demo5/src/main/java/com/xiaotu/mvc/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.xiaotu.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/")
+    public String testIndex(){
+        return "index";
+    }
+
+
+    @RequestMapping("/**/testInterceptor")
+    public String testInterceptor(){
+        return "success";
+    }
+
+    @RequestMapping("/testExceptionHandler")
+    public String testExceptionHandler(){
+        System.out.println(1/0);
+        return "success";
+    }
+
+}
